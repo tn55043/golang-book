a477d91d1f36b007e53bc8a68817bcc5782c968c
tn55043 <parisa.th@tnis.com>
2018-02-21 10:30:11 +0700

    Add chapter9-1

--- chapter9-1/main.go
+++ chapter9-1/main.go
@@ -11,11 +11,25 @@ type Circle struct {
 }
 
 func main() {
+	// Zero Value
 	var c Circle
 	fmt.Printf("c type: %T\n", c)
 	fmt.Println(c.x, c.y, c.r)
-	
+
 	c1 := new(Circle)
 	fmt.Printf("c1 type: %T\n", c1)
 	fmt.Println(c1.x, c1.y, c1.r)
+
+	// Insert Value
+	c2 := Circle{x: 0, y: 0, r: 5}
+	fmt.Printf("c2 type: %T\n", c2)
+	fmt.Println(c2.x, c2.y, c2.r)
+
+	c3 := NewCircle(1, 2, 3)
+	fmt.Printf("c3 type: %T\n", c3)
+	fmt.Println(c3.x, c3.y, c3.r)
+}
+
+func NewCircle(x, y, r float64) *Circle {
+	return &Circle{x, y, r}
 }
