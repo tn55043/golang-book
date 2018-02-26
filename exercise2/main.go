df6286635ba340d36183375f56c63337ed386412
tn55043 <parisa.th@tnis.com>
2018-02-26 12:26:56 +0700

    Add exercise2

--- exercise2/main.go
+++ exercise2/main.go
@@ -46,25 +46,46 @@ func (m *VendingMachine) change(c int) string {
 	var str string
 	values := [...]int{10, 5, 2, 1}
 	coins := [...]string{"T", "F", "TW", "O"}
-	if c >= values[0] {
-		str += ", " + coins[0]
-		c -= values[0]
+
+	for i := 0; i < len(values); i++ {
+		if c >= values[i] {
+			str += ", " + coins[i]
+			c -= values[i]
+			i--
+		}
 	}
-	if c >= values[1] {
-		str += ", " + coins[1]
-		c -= values[1]
-	}
-	if c >= values[2] {
-		str += ", " + coins[2]
-		c -= values[2]
-	}
+	/*
+		if c >= values[0] {
+			str += ", " + coins[0]
+			c -= values[0]
+		}
+		if c >= values[1] {
+			str += ", " + coins[1]
+			c -= values[1]
+		}
+		if c >= values[2] {
+			str += ", " + coins[2]
+			c -= values[2]
+		}
+		if c >= values[3] {
+			str += ", " + coins[3]
+			c -= values[3]
+		}
+	*/
 	return str
+}
+
+func (vm *VendingMachine) CoinReturn() string {
+	coins := vm.change(vm.insertedMoney)
+	vm.insertedMoney = 0
+	return coins[2:len(coins)]
 }
 
 func main() {
 	var coins = map[string]int{"T": 10, "F": 5, "TW": 2, "O": 1}
 	var items = map[string]int{"SD": 18, "CC": 12, "DW": 7}
 	vm := VendingMachine{coins: coins, items: items}
+	
 	// Inserted Money = 0
 	fmt.Println("Inserted Money :", vm.InsertedMoney())
 
@@ -89,5 +110,13 @@ func main() {
 	// Inserted Money = 20
 	fmt.Println("Inserted Money :", vm.InsertedMoney())
 	can = vm.SelectCC()
+	fmt.Println(can)
+
+	vm.InsertCoin("T")
+	vm.InsertCoin("T")
+	vm.InsertCoin("F")
+	// Inserted Money = 25
+	fmt.Println("Inserted Money :", vm.InsertedMoney())
+	can = vm.CoinReturn()
 	fmt.Println(can)
 }
