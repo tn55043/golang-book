8d760bbc1f10eaa220e4469338b7e084bf7a31cc
tn55043 <parisa.th@tnis.com>
2018-03-05 11:11:20 +0700

    Add chapter12-3

--- chapter12-3/main.go
+++ chapter12-3/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
@@ -10,7 +11,47 @@ func main() {
 			return a + b
 		}
 	}
-
 	addNewFunc := addFunc(2)
 	fmt.Println(addNewFunc(3))
+
+	// FizzBuzz Program
+	for i := 0; i < 100; i++ {
+		fmt.Println(i, fizzbuzz(i))
+	}
+}
+
+func fizzbuzz(number int) string {
+	fizzbuzzFunc := func(n int) (string, bool) {
+		if n%15 == 0 {
+			return "FizzBuzz", true
+		}
+		return "", false
+	}
+
+	fizzFunc := func(n int) (string, bool) {
+		if n%3 == 0 {
+			return "Fizz", true
+		}
+		return "", false
+	}
+
+	buzzFunc := func(n int) (string, bool) {
+		if n%5 == 0 {
+			return "Buzz", true
+		}
+		return "", false
+	}
+
+	fbArrey := [...]func(n int) (string, bool){
+		fizzbuzzFunc,
+		fizzFunc,
+		buzzFunc,
+	}
+
+	for i := 0; i < len(fbArrey); i++ {
+		if str, ok := fbArrey[i](number); ok {
+			return str
+		}
+	}
+	return strconv.Itoa(number)
 }
