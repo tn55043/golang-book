a2bc9f85609927f7a3894e562f962e17774512f8
tn55043 <parisa.th@tnis.com>
2018-05-14 11:45:03 +0700

    Add example1

--- panic/main.go
+++ panic/main.go
@@ -4,6 +4,73 @@ import (
 	"fmt"
 )
 
+func f(ch chan int) {
+	defer func() {
+		fmt.Println("Deferred by f")
+	}()
+	g()
+	ch <- 0
+}
+
+func g() {
+	defer func() {
+		fmt.Println("Deferred by g")
+	}()
+	h()
+}
+
+func h() {
+	defer func() {
+		fmt.Println("Deferred by h")
+	}()
+	panic("boom!")
+}
+
+func main() {
+	defer func() {
+		fmt.Println("recover")
+		if p := recover(); p != nil {
+			fmt.Printf("Panic found: %v\n", p)
+		}
+	}()
+	ch := make(chan int)
+	go f(ch)
+	<-ch
+}
+
+/*
+func f() {
+	defer func() {
+		fmt.Println("Deferred by f")
+	}()
+	g()
+}
+
+func g() {
+	defer func() {
+		fmt.Println("Deferred by g")
+	}()
+	h()
+}
+
+func h() {
+	defer func() {
+		fmt.Println("Deferred by h")
+	}()
+	panic("boom!")
+}
+
+func main() {
+	defer func() {
+		fmt.Println("recover")
+		if p := recover(); p != nil {
+			fmt.Printf("Panic found: %v\n", p)
+		}
+	}()
+	f()
+}
+*/
+/*
 func main() {
 	defer func() {
 		fmt.Println("recover")
@@ -15,3 +82,4 @@ func main() {
 	fmt.Println("start main")
 	panic("boom!")
 }
+*/
